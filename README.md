d324ec150d1f7f9b73e633525e9f7367c5bccb16
Tanya Gray <tgratgra@gmail.com>
2022-09-21 22:11:45 +0100

    add

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# linkedart-exhibition-browser
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ interfaces/index.ts
@@ -0,0 +1,8 @@
+export type Event = {
+    id: number
+    filename:string
+    _label?: string
+    start?: Date
+    end?: Date
+  }
+  
